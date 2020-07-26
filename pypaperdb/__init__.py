3217d8aa989e562ac561932ad924506d0d6c948a
Sonja <scythja@gmail.com>
2020-07-26 22:35:21 +1000

    quotes added to version string

--- pypaperdb/__init__.py
+++ pypaperdb/__init__.py
@@ -2,4 +2,4 @@ from pypaperdb import application, custom, misc
 
 __authors__ = 'Edwin G.W. Peters and Sonja Stuedli'
 __author_emails__ = 'edwin.g.w.peters@gmail.com, scythja@gmail.com'
-__version__ = 0.8.0
+__version__ = '0.8.0'
